4b6a90accafdb2a032d708ab253beddf0d7ab1e5
jjanmo <jjanmo07@hanmail.net>
2020-05-23 14:09:41 +0900

    Update tictactoe

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -2,19 +2,68 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button className="square">
-                {/* TODO */}
-            </button>
-        );
-    }
+//Square 컴퍼넌트를 클래스 컴퍼넌트로 구성할 필요가 없다
+//-> 함수 컴퍼넌트로 변경
+// class Square extends React.Component { //제어되는 컴퍼넌트(controlled component)
+//     // constructor(props) {
+//     //     super(props);   //클래스 컴퍼넌트는 생성자를 가질 때, 항상 super(props)를 작성해야한다 
+//     //     this.state = {  //컴퍼넌트에 state 생성 
+//     //         value: null
+//     //     }
+//     // }
+//     //=> Square는 게임상태를 유지할 필요가 없기때문에 state도 사실 필요없다
+
+//     render() {
+//         return (
+//             <button
+//                 className="square"
+//                 onClick={
+//                     () => this.props.onClick()
+//                 }
+//             /*
+//             1. 버튼클릭
+//             2. Board에서 props로 넘어온 onClick()호출
+//             3. Board에 있는 onClick의 handleClick()호출
+
+//             */
+//             >
+//                 {this.props.value}
+//             </button >
+//         );
+//     }
+// }
+
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
+
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { squares: Array(9).fill(null) };
+    }
+
+    handleClick(i) {
+        //참조끊고 새로운 배열 생성 : 
+        const squares = this.state.squares.slice();
+        //데이터 업데이트
+        squares[i] = 'X';
+        //상태변경
+        this.setState({ squares });
+    }
+
     renderSquare(i) {
-        return <Square />;
+        return <Square
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+        //Board => Square로 함수를 props 전달  
+        //onClick props는 Square를 클릭하면 호출되는 함수
+        />;
     }
 
     render() {
@@ -59,7 +108,6 @@ class Game extends React.Component {
     }
 }
 
-// ========================================
 
 ReactDOM.render(
     <Game />,
