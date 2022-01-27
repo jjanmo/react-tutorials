1dec1ead043d615ac9d574666151b480b16db99f
jjanmo <jjanmo07@hanmail.net>
2022-01-27 17:20:46 +0900

    feat: add form component using typescript

--- project03/src/app.tsx
+++ project03/src/app.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
-import Circle from './circle';
+import React, { useCallback, useState } from 'react';
+
+type FormData = {
+  id: string;
+  password: string;
+};
 
 const App = () => {
+  const [data, setData] = useState<FormData>({
+    id: '',
+    password: '',
+  });
+
+  const onSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      if (data.id && data.password) {
+        console.log('submit');
+        alert(`${data.id} | ${data.password}`);
+      }
+    },
+    [data]
+  );
+
+  const onChange = useCallback(
+    (e: React.FormEvent<HTMLInputElement>) => {
+      const { name, value } = e.currentTarget;
+
+      setData({
+        ...data,
+        [name]: value,
+      });
+    },
+    [data]
+  );
+
   return (
     <div>
-      <Circle bgColor="tomato" />
-      <Circle bgColor="yellowgreen" />
+      <form onSubmit={onSubmit}>
+        <input type="text" name="id" placeholder="Enter your ID" onChange={onChange} />
+        <input type="password" name="password" placeholder="Enter your password" onChange={onChange} />
+        <button>LOGIN</button>
+      </form>
     </div>
   );
 };
