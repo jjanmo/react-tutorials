6a1252c4c7108bc9d9247667acc3707d3f8cbc7f
jjanmo <jjanmo07@hanmail.net>
2022-02-03 23:33:09 +0900

    feat: add date util function

--- project03/src/utils/functions.ts
+++ project03/src/utils/functions.ts
@@ -3,3 +3,17 @@ export const addComma = (number: number | undefined): string => {
 
   return number.toLocaleString();
 };
+
+export const getBeforeWeek = (): string[] => {
+  const now = new Date();
+
+  const covertMonthFormat = (month: number): string => (month > 8 ? `${month + 1}` : `0${month + 1}`);
+  const covertDateFormat = (date: number) => (date > 9 ? `${date}` : `0${date}`);
+
+  const end = `${now.getFullYear()}-${covertMonthFormat(now.getMonth())}-${covertDateFormat(now.getDate())}`;
+
+  now.setDate(now.getDate() - 6);
+  const start = `${now.getFullYear()}-${covertMonthFormat(now.getMonth())}-${covertDateFormat(now.getDate())}`;
+
+  return [start, end];
+};
