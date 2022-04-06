fa8b55c95f12191b55994b1c8e6de739331d97cd
jjanmo <jjanmo07@hanmail.net>
2022-04-07 00:03:12 +0900

    feat: add primary colors

---
+++ project05/src/style/colors.js
@@ -0,0 +1,15 @@
+const COLOR = {
+  PRIMARY_010: '#e36c61',
+  PRIMARY_020: '#d79a93',
+  PRIMARY_030: '#ece2d9',
+  PRIMARY_040: '#736a65',
+  PRIMARY_050: '#513d27',
+  PRIMARY_060: '#383c44',
+  PRIMARY_070: '#f2b549',
+  PRIMARY_080: '#fcd46c',
+  PRIMARY_090: '#947c34',
+  PRIMARY_100: '#60948c',
+  PRIMARY_110: '#a0d1ce',
+};
+
+export default COLOR;
