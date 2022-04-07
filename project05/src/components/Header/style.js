6264092915402ca0c7957b41d7879919b8cf269e
jjanmo <jjanmo07@hanmail.net>
2022-04-08 01:33:09 +0900

    feat: update logout button with animation

--- project05/src/components/Header/style.js
+++ project05/src/components/Header/style.js
@@ -1,9 +1,10 @@
+import { Link } from 'react-router-dom';
 import COLOR from 'style/colors';
 import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
-  height: 5rem;
+  height: 6rem;
   padding: 0 2rem;
   margin: 1rem 0;
   border-radius: 1rem;
@@ -18,7 +19,41 @@ export const Container = styled.div`
   background-color: ${COLOR.PRIMARY_040};
 `;
 
-export const LinkWrapper = styled.div`
+export const LogoutBtn = styled.button`
+  position: relative;
+  padding: 0.8rem 4rem;
+  border-radius: 0.2rem;
+
+  outline: none;
+  border: none;
+
+  background-color: ${COLOR.PRIMARY_060};
+  color: ${COLOR.WHITE};
+
+  cursor: pointer;
+  text-transform: uppercase;
+  font-weight: 700;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 50%;
+    transform: translate(-50%);
+    width: 0%;
+    height: 100%;
+    background-color: ${COLOR.PRIMARY_070};
+    transition: all 0.5s ease;
+    transform-origin: center;
+    opacity: 0.6;
+  }
+
+  &:hover:before {
+    width: 100%;
+  }
+`;
+
+export const ButtonWrapper = styled.div`
   width: 25%;
 
   display: flex;
