e105c6bcaa0b907e4929a73511951b98dea59bc6
jjanmo <jjanmo07@hanmail.net>
2022-04-08 01:43:17 +0900

    feat: add icon using fontawesome

---
+++ project05/src/icons/Signin/Signin.js
@@ -0,0 +1,8 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons';
+
+function Signin() {
+  return <FontAwesomeIcon icon={faArrowRightToBracket} />;
+}
+
+export default Signin;
---
+++ project05/src/icons/Signup/Signup.js
@@ -0,0 +1,8 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
+
+function Signup() {
+  return <FontAwesomeIcon icon={faUserPlus} />;
+}
+
+export default Signup;
