1d02095849a586b0b50c13bba32141b203fe71fe
jjanmo <jjanmo07@hanmail.net>
2022-04-08 22:24:26 +0900

    feat: update icon style

--- project05/src/icons/Signin/Signin.js
+++ project05/src/icons/Signin/Signin.js
@@ -1,8 +1,20 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
 
 function Signin() {
-  return <FontAwesomeIcon icon={faArrowRightToBracket} />;
+  return <StyledIcon icon={faArrowRightToBracket} />;
 }
 
 export default Signin;
+
+const StyledIcon = styled(FontAwesomeIcon)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 15%;
+  height: 100%;
+  padding: 0 1rem;
+  background-color: #d79a93;
+  z-index: 10;
+`;
--- project05/src/icons/Signup/Signup.js
+++ project05/src/icons/Signup/Signup.js
@@ -1,8 +1,20 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
 
 function Signup() {
-  return <FontAwesomeIcon icon={faUserPlus} />;
+  return <StyledIcon icon={faUserPlus} />;
 }
 
 export default Signup;
+
+const StyledIcon = styled(FontAwesomeIcon)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 15%;
+  height: 100%;
+  padding: 0 1rem;
+  background-color: #a0d1ce;
+  z-index: 10;
+`;
