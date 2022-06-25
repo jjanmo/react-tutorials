7fa73b6a1293f2d56b5794654b2f095280da24a4
jjanmo <jjanmo07@hanmail.net>
2022-06-25 22:42:01 +0900

    feat: add button logic test code

--- project06/src/Counter/Counter.test.tsx
+++ project06/src/Counter/Counter.test.tsx
@@ -1,5 +1,5 @@
 import Counter from './Counter'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 
 test('the counter starts at 0', () => {
   render(<Counter />)
@@ -17,4 +17,20 @@ test('minus button has correct text', () => {
   render(<Counter />)
   const minusBtnElem = screen.getByTestId('minus-btn')
   expect(minusBtnElem).toHaveTextContent('-')
+})
+
+test('click plus button, add one(from 0 to 1)', () => {
+  render(<Counter />)
+  const plusBtnElem = screen.getByTestId('plus-btn')
+  const countElem = screen.getByTestId('count')
+  fireEvent.click(plusBtnElem)
+  expect(countElem).toHaveTextContent('1')
+})
+
+test('click minus button, subtract one(from 0 to -1)', () => {
+  render(<Counter />)
+  const minusBtnElem = screen.getByTestId('minus-btn')
+  const countElem = screen.getByTestId('count')
+  fireEvent.click(minusBtnElem)
+  expect(countElem).toHaveTextContent('-1')
 })
--- project06/src/Counter/Counter.tsx
+++ project06/src/Counter/Counter.tsx
@@ -3,12 +3,19 @@ import { useState } from 'react'
 function Counter() {
   const [count, setCount] = useState(0)
 
+  const handleClickPlus = () => setCount(count + 1)
+  const handleClickMinus = () => setCount(count - 1)
+
   return (
     <div>
       <h1 data-testid="count">{count}</h1>
       <div className="controls">
-        <button data-testid="plus-btn">+</button>
-        <button data-testid="minus-btn">-</button>
+        <button data-testid="plus-btn" onClick={handleClickPlus}>
+          +
+        </button>
+        <button data-testid="minus-btn" onClick={handleClickMinus}>
+          -
+        </button>
       </div>
     </div>
   )
