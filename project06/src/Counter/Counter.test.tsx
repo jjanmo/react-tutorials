2d9daf716fa3bf1c77ed6b984d5049b5082eadef
jjanmo <jjanmo07@hanmail.net>
2022-06-25 22:16:03 +0900

    feat: add button test code

--- project06/src/Counter/Counter.test.tsx
+++ project06/src/Counter/Counter.test.tsx
@@ -6,3 +6,15 @@ test('the counter starts at 0', () => {
   const countElem = screen.getByTestId('count')
   expect(countElem).toHaveTextContent('0')
 })
+
+test('plus button has correct text', () => {
+  render(<Counter />)
+  const plusBtnElem = screen.getByTestId('plus-btn')
+  expect(plusBtnElem).toHaveTextContent('+')
+})
+
+test('minus button has correct text', () => {
+  render(<Counter />)
+  const minusBtnElem = screen.getByTestId('minus-btn')
+  expect(minusBtnElem).toHaveTextContent('-')
+})
--- project06/src/Counter/Counter.tsx
+++ project06/src/Counter/Counter.tsx
@@ -6,6 +6,10 @@ function Counter() {
   return (
     <div>
       <h1 data-testid="count">{count}</h1>
+      <div className="controls">
+        <button data-testid="plus-btn">+</button>
+        <button data-testid="minus-btn">-</button>
+      </div>
     </div>
   )
 }
