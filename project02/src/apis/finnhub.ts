d551828033998bed31fefbb59fa77c3c9b7588de
jjanmo <jjanmo07@hanmail.net>
2022-09-25 13:05:36 +0900

    feat: add apis from open-api(paprika,finnhub)

---
+++ project02/src/apis/finnhub.ts
@@ -0,0 +1,21 @@
+import { FINNHUB_API } from '../apis';
+
+type ResolutionType = 1 | 5 | 15 | 30 | 60 | 'D' | 'W' | 'M';
+
+// 주말 데이터가 빠짐 🤯
+export const fetchOHLCData = ({
+  symbol,
+  resolution,
+  from,
+  to,
+  token = process.env.API_KEY,
+}: {
+  symbol: string;
+  resolution: ResolutionType;
+  from: number;
+  to: number;
+  token?: string;
+}) =>
+  FINNHUB_API.get(
+    `/stock/candle?symbol=${symbol}&resolution=${resolution}&from=${from}&to=${to}&token=${token}`
+  ).then((res) => res.data);
--- project02/src/apis/index.ts
+++ project02/src/apis/index.ts
@@ -1,24 +1,9 @@
 import axios from 'axios';
-import { Coin } from '../types/coin';
-import { Ticker } from '../types/ticker';
 
-const BASE_URL = 'https://api.coinpaprika.com/v1';
+export const PAPRIKA_API = axios.create({
+  baseURL: 'https://api.coinpaprika.com/v1',
+});
 
-export const fetchTickers = () =>
-  axios.get<Ticker[]>(`${BASE_URL}/tickers`).then((res) => res.data);
-
-export const fetchCoinById = (id: string) =>
-  axios.get<Coin>(`${BASE_URL}/coins/${id}`).then((res) => res.data);
-
-export const fetchTickerById = (id: string) =>
-  axios.get<Ticker>(`${BASE_URL}/tickers/${id}`).then((res) => res.data);
-
-// export function fetchOHLCData(
-//   id: string | undefined,
-//   start: string,
-//   end: string
-// ): Promise<IOHLCData[]> {
-//   return fetch(
-//     `https://api.coinpaprika.com/v1/coins/${id}/ohlcv/historical?start=${start}&end=${end}`
-//   ).then((response) => response.json());
-// }
+export const FINNHUB_API = axios.create({
+  baseURL: 'https://finnhub.io/api/v1',
+});
