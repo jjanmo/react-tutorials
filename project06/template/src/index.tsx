c29c8988921af8ecd2ea693b4b3f60ad853cd91a
jjanmo <jjanmo07@hanmail.net>
2022-10-22 00:02:59 +0900

    feat: add infinit scroll project

--- project06/template/src/index.tsx
+++ project06/template/src/index.tsx
@@ -5,11 +5,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const client = new QueryClient();
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
-const client = new QueryClient();
 
 root.render(
   <React.StrictMode>
