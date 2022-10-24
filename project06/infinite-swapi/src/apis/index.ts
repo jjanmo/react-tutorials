26e1c3e0bf561b4824681694a1aa87bb96fed3cf
jjanmo <jjanmo07@hanmail.net>
2022-10-25 00:56:16 +0900

    feat: add people api

--- project06/infinite-swapi/src/apis/index.ts
+++ project06/infinite-swapi/src/apis/index.ts
@@ -1,9 +1,12 @@
 import axios from 'axios';
+import * as T from './types';
 
 const BASE_URL = 'https://swapi.dev/api/';
 
 export const fetchPeople = async () =>
-  await axios.get(`${BASE_URL}/people`).then((res) => res.data);
+  await axios
+    .get<T.FetchPeopleResponse>(`${BASE_URL}/people`)
+    .then((res) => res.data);
 
 export const fetchStarShips = async () =>
   await axios.get(`${BASE_URL}/starships`).then((res) => res.data);
--- project06/infinite-swapi/src/pages/PeopleScroll/PeopleScroll.tsx
+++ project06/infinite-swapi/src/pages/PeopleScroll/PeopleScroll.tsx
@@ -1,5 +1,24 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { fetchPeople } from '@apis/index';
+
 function PeopleScroll() {
-  return <div>This is InfiniteScroll</div>;
+  const { data, hasNextPage, fetchNextPage } = useInfiniteQuery(
+    ['sw-people'],
+    fetchPeople,
+    {
+      getNextPageParam: (currentPageData) => currentPageData.next,
+    }
+  );
+
+  console.log('🙏', data, hasNextPage, fetchNextPage);
+
+  return (
+    <div>
+      {data?.pages.map((page) =>
+        page.results.map((people) => <div>{people.name}</div>)
+      )}
+    </div>
+  );
 }
 
 export default PeopleScroll;
