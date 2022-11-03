01429c73979aaa3c723d117a5383e1c2cb857d2e
jjanmo <jjanmo07@hanmail.net>
2022-11-03 23:45:03 +0900

    feat: mousemove event

--- project07/src/components/Card/Card.style.ts
+++ project07/src/components/Card/Card.style.ts
@@ -6,30 +6,34 @@ export const Scene = styled.div`
   left: 50%;
   transform: translate(-50%, -50%);
 
+  perspective: 600px;
+`;
+export const Card = styled.div<{ x?: number; y?: number }>`
   width: 330px;
   height: 462px;
   border-radius: 10px;
   overflow: hidden;
 
-  perspective: 600px;
-`;
-export const Card = styled.div`
-  width: 100%;
-  height: 100%;
-
   transform-style: preserve-3d;
+  transform-origin: center center;
+
+  transform: ${({ x, y }) => `rotateX(${x}deg) rotateY(${y}deg)`};
 `;
-export const Back = styled.div<{ x?: number; y?: number }>`
+export const Back = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
   width: 100%;
   height: 100%;
   background: url(${process.env.PUBLIC_URL}/images/back.jpg) no-repeat center
     center;
   background-size: contain;
-
-  transform: ${({ x, y }) => `rotate3d(${x} / 165, ${y} / 231, 0)`};
 `;
 
 export const Front = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
   width: 100%;
   height: 100%;
   background: url(${process.env.PUBLIC_URL}/images/front.png) no-repeat center
--- project07/src/components/Card/Card.tsx
+++ project07/src/components/Card/Card.tsx
@@ -6,17 +6,23 @@ type CardPosition = {
   y: number;
 };
 
+const speed = 0.09;
 export default function Card() {
   const card = useRef<HTMLDivElement>(null);
+  const position = useRef({
+    x: 0,
+    y: 0,
+  });
   const [center, setCenter] = useState({
     x: 0,
     y: 0,
   });
-  const [cardPos, setCardPos] = useState<CardPosition | null>(null);
 
-  // card.current?.addEventListener('mousemove', (e: React.MouseEvent) => {
-  //   console.log(e.clientX, e.clientY);
-  // });
+  const [cardPos, setCardPos] = useState<CardPosition>({
+    x: 0,
+    y: 0,
+  });
+
   useEffect(() => {
     const handleResize = () => {
       setCenter({
@@ -25,23 +31,44 @@ export default function Card() {
       });
     };
     handleResize();
-
     window.addEventListener('resize', handleResize);
 
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  const handleMousemove = (e: React.MouseEvent) => {
+  useEffect(() => {
     setCardPos({
-      x: e.clientX - center.x,
-      y: e.clientY - center.y,
+      x: cardPos.x + (position.current.x - cardPos.x) * speed,
+      y: cardPos.y + (position.current.y - cardPos.y) * speed,
     });
+  }, [position.current.x, position.current.y, cardPos]);
+
+  const handleMousemove = (e: React.MouseEvent) => {
+    position.current = {
+      x: (e.clientX - center.x) / 5,
+      y: -(e.clientY - center.y) / 5,
+    };
+  };
+
+  const handleMouseOut = () => {
+    setTimeout(() => {
+      position.current = {
+        x: 0,
+        y: 0,
+      };
+    }, 800);
   };
 
   return (
     <S.Scene>
-      <S.Card ref={card} onMouseMove={handleMousemove}>
-        <S.Back x={cardPos?.x} y={cardPos?.y} />
+      <S.Card
+        ref={card}
+        x={cardPos.x}
+        y={cardPos.y}
+        onMouseMove={handleMousemove}
+        onMouseOut={handleMouseOut}
+      >
+        <S.Back />
         <S.Front />
       </S.Card>
     </S.Scene>
