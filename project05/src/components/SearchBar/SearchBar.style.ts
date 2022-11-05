2f98aed849459e72932698bcffdcec4ec37da463
jjanmo <jjanmo07@hanmail.net>
2022-11-05 16:03:49 +0900

    feat: add search-bar

---
+++ project05/src/components/SearchBar/SearchBar.style.ts
@@ -0,0 +1,13 @@
+import { Box, styled, TextField } from '@mui/material'
+
+export const SBox = styled(Box)`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 2rem 1rem;
+
+  border-bottom: 1px solid black;
+`
+export const STextField = styled(TextField)`
+  width: 80%;
+`
---
+++ project05/src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,19 @@
+import { Box, Button, Container, TextField } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search'
+import * as S from './SearchBar.style'
+
+export default function SearchBar() {
+  return (
+    <S.SBox>
+      <S.STextField
+        type="text"
+        label="Search"
+        size="small"
+        InputProps={{
+          endAdornment: <SearchIcon />,
+        }}
+      />
+      <Button variant="outlined">New</Button>
+    </S.SBox>
+  )
+}
