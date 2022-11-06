64740de1c81e3cc3c7545b0235c6ab42f35cffd5
jjanmo <jjanmo07@hanmail.net>
2022-11-06 23:45:05 +0900

    feat: add card hover animation

--- project07/src/components/Card/Card.style.ts
+++ project07/src/components/Card/Card.style.ts
@@ -40,3 +40,34 @@ export const Front = styled.div`
     center;
   background-size: contain;
 `;
+export const Illusion = styled.div`
+  width: 100%;
+  height: 100%;
+
+  background-image: url(${process.env.PUBLIC_URL}/images/illusion.png),
+    repeating-linear-gradient(
+      0deg,
+      rgb(255, 119, 115) calc(var(--space) * 1),
+      rgba(255, 237, 95, 1) calc(var(--space) * 2),
+      rgba(168, 255, 95, 1) calc(var(--space) * 3),
+      rgba(131, 255, 247, 1) calc(var(--space) * 4),
+      rgba(120, 148, 255, 1) calc(var(--space) * 5),
+      rgb(216, 117, 255) calc(var(--space) * 6),
+      rgb(255, 119, 115) calc(var(--space) * 7)
+    ),
+    repeating-linear-gradient(
+      var(--angle),
+      #0e152e 0%,
+      hsl(180, 10%, 60%) 3.8%,
+      hsl(180, 29%, 66%) 4.5%,
+      hsl(180, 10%, 60%) 5.2%,
+      #0e152e 10%,
+      #0e152e 12%
+    ),
+    radial-gradient(
+      farthest-corner circle at var(--mx) var(--my),
+      rgba(0, 0, 0, 0.1) 12%,
+      rgba(0, 0, 0, 0.15) 20%,
+      rgba(0, 0, 0, 0.25) 120%
+    );
+`;
--- project07/src/pages/home/Home.tsx
+++ project07/src/pages/home/Home.tsx
@@ -1,7 +1,26 @@
+import * as S from './Home.style';
+
 export default function Home() {
   return (
-    <div>
-      <div></div>
-    </div>
+    <S.Container>
+      <S.Scene>
+        <S.Item>
+          <S.Content>Number1</S.Content>
+        </S.Item>
+        <S.Item>
+          <S.Content>Number2</S.Content>
+        </S.Item>
+        <S.Item>
+          <S.Guide>Hover me</S.Guide>
+          <S.Content>Number3</S.Content>
+        </S.Item>
+        <S.Item>
+          <S.Content>Number4</S.Content>
+        </S.Item>
+        <S.Item>
+          <S.Content>Number5</S.Content>
+        </S.Item>
+      </S.Scene>
+    </S.Container>
   );
 }
