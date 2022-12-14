cf95b5d40725d6466244e7d2901ae7540838412b
jjanmo <jjanmo07@hanmail.net>
2022-12-14 23:38:00 +0900

    feat: make draggable list

--- project05/src/pages/rbd/rbd.tsx
+++ project05/src/pages/rbd/rbd.tsx
@@ -1,3 +1,51 @@
-export default function DNDBoard() {
-  return <div>This is React Beautiful Dnd</div>
+import { List, ListItem } from '@mui/material'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
+import draggableIcon from '../../assets/icons/draggable.png'
+
+export default function Rbd() {
+  const todos = [
+    {
+      id: 1,
+      content: '자바스크립트 공부하기',
+    },
+    {
+      id: 2,
+      content: 'D3 튜터리얼 하기!',
+    },
+    {
+      id: 3,
+      content: 'CSS 공부하기!',
+    },
+  ]
+
+  const handleDragEnd = () => {}
+
+  return (
+    <DragDropContext onDragEnd={handleDragEnd}>
+      <div>
+        <Droppable droppableId="1">
+          {(provided, snapshot) => (
+            <List ref={provided.innerRef} {...provided.droppableProps}>
+              {todos.map((todo) => (
+                <Draggable key={todo.id} draggableId={`${todo.id}`} index={todo.id}>
+                  {(provided, snapshot) => (
+                    <ListItem ref={provided.innerRef} {...provided.draggableProps}>
+                      <img
+                        src={draggableIcon}
+                        {...provided.dragHandleProps}
+                        alt="draggable-icon"
+                        width={20}
+                        height={50}
+                      />
+                      {todo.content}
+                    </ListItem>
+                  )}
+                </Draggable>
+              ))}
+            </List>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
+  )
 }
