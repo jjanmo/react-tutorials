3f3099bd2ff0e80dec0716d91a645be7d5dffaf5
jjanmo <jjanmo07@hanmail.net>
2022-12-15 00:12:30 +0900

    feat: update ui using material ui

--- project05/src/pages/rbd/rbd.tsx
+++ project05/src/pages/rbd/rbd.tsx
@@ -1,6 +1,6 @@
-import { List, ListItem } from '@mui/material'
+import { Container, List, ListItem, ListItemIcon } from '@mui/material'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
-import draggableIcon from '../../assets/icons/draggable.png'
+import DragHandleIcon from '@mui/icons-material/DragHandle'
 
 export default function Rbd() {
   const todos = [
@@ -21,31 +21,46 @@ export default function Rbd() {
   const handleDragEnd = () => {}
 
   return (
-    <DragDropContext onDragEnd={handleDragEnd}>
-      <div>
-        <Droppable droppableId="1">
-          {(provided, snapshot) => (
-            <List ref={provided.innerRef} {...provided.droppableProps}>
-              {todos.map((todo) => (
-                <Draggable key={todo.id} draggableId={`${todo.id}`} index={todo.id}>
-                  {(provided, snapshot) => (
-                    <ListItem ref={provided.innerRef} {...provided.draggableProps}>
-                      <img
-                        src={draggableIcon}
-                        {...provided.dragHandleProps}
-                        alt="draggable-icon"
-                        width={20}
-                        height={50}
-                      />
-                      {todo.content}
-                    </ListItem>
-                  )}
-                </Draggable>
-              ))}
-            </List>
-          )}
-        </Droppable>
-      </div>
-    </DragDropContext>
+    <Container
+      sx={{
+        backgroundColor: '#4C9AFF',
+        width: '100vw',
+        minHeight: 'calc(100vh - 60px)',
+        padding: '1rem',
+      }}
+    >
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <Container
+          sx={{
+            backgroundColor: '#EBECF0',
+            width: '300px',
+            minHeight: '20vh',
+            margin: 0,
+          }}
+        >
+          <Droppable droppableId="1">
+            {(provided, snapshot) => (
+              <List ref={provided.innerRef} {...provided.droppableProps}>
+                {todos.map((todo) => (
+                  <Draggable key={todo.id} draggableId={`${todo.id}`} index={todo.id}>
+                    {(provided, snapshot) => (
+                      <ListItem ref={provided.innerRef} {...provided.draggableProps}>
+                        <ListItemIcon
+                          {...provided.dragHandleProps}
+                          sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+                        >
+                          <DragHandleIcon />
+                        </ListItemIcon>
+                        {todo.content}
+                      </ListItem>
+                    )}
+                  </Draggable>
+                ))}
+              </List>
+            )}
+          </Droppable>
+        </Container>
+      </DragDropContext>
+    </Container>
   )
 }
