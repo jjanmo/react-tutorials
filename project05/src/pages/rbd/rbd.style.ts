00524633e1c5d3312740a3bee8897acb63a64591
jjanmo <jjanmo07@hanmail.net>
2022-12-16 22:45:38 +0900

    fix: seperete style file

---
+++ project05/src/pages/rbd/rbd.style.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled'
+import { Container, ListItemIcon } from '@mui/material'
+
+export const SContainer = styled(Container)`
+  background-color: #4c9aff;
+  width: 100vw;
+  min-height: calc(100vh - 60px);
+  padding: 1rem;
+`
+export const SListItemIcon = styled(ListItemIcon)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
