a9534da449b5ee21a8a7465d045aa2523e45976d
jjanmo <jjanmo07@hanmail.net>
2023-01-21 20:07:06 +0900

    fix: fix routes between loggedin and not

--- project03/src/AppRouter.tsx
+++ project03/src/AppRouter.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from '@pages/Home'
 import SignIn from '@pages/SignIn'
@@ -5,13 +6,25 @@ import SignUp from '@pages/SignUp'
 import Layout from '@components/Layout'
 
 function AppRouter() {
+  const [loggedIn, setLoggedIn] = useState<boolean>(false)
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="signup" element={<SignUp />} />
-          <Route path="signin" element={<SignIn />} />
+          {loggedIn ? (
+            <>로그아웃 </>
+          ) : (
+            <>
+              <Route path="signup" element={<SignUp />} />
+              <Route path="signin" element={<SignIn />} />
+            </>
+          )}
+
+          {/* <Route path="talk" element={<SignIn />} />
+          <Route path="todos" element={<SignIn />} />
+          <Route path="profile" element={<SignIn />} /> */}
         </Route>
       </Routes>
     </Router>
