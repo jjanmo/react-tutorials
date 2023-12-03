0810f9cd5150db28d6f0a06dd42be4935cc97246
jjanmo <jjanmo07@hanmail.net>
2023-12-03 23:07:10 +0900

    fix: 이벤트 핸들러 수정

--- project06/client/src/components/ChatRoom.tsx
+++ project06/client/src/components/ChatRoom.tsx
@@ -1,28 +1,48 @@
-import { useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import * as S from './ChatRoom.styled';
+import useWebSocket from '../hooks/useWebSocket';
+
+export type Message = {
+  text: string;
+  timestamp: string;
+};
 
 export default function ChatRoom() {
-  const [messages, setMessages] = useState([]);
   const [messageInput, setMessageInput] = useState('');
+  const [messages, setMessages] = useState<Message[]>([]);
+  const { webSocket } = useWebSocket({ setMessages });
 
-  const handleClick = () => {};
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setMessageInput(value);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (messageInput.trim() !== '' && webSocket) {
+      const message: Message = {
+        text: messageInput,
+        timestamp: new Date().toISOString(),
+      };
+      webSocket.send(JSON.stringify(message));
+      setMessageInput('');
+    }
+  };
 
   return (
     <S.Container>
       <S.ChatContainer>
         <S.Messages>
           {messages.map((message, index) => (
-            <S.Message key={index}>{message}</S.Message>
+            <S.Message key={index}>{message.text}</S.Message>
           ))}
         </S.Messages>
+
         <S.InputContainer>
-          <S.Input
-            type="text"
-            placeholder="Type your message..."
-            value={messageInput}
-            onChange={(e) => setMessageInput(e.target.value)}
-          />
-          <S.Button onClick={handleClick}>Send</S.Button>
+          <S.Input type="text" placeholder="Type your message..." value={messageInput} onChange={handleChange} />
+          <S.Button type="submit" onClick={handleSubmit}>
+            Send
+          </S.Button>
         </S.InputContainer>
       </S.ChatContainer>
     </S.Container>
