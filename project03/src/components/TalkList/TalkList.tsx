51f56c446ab859ce1a3759020b1a71d3c32a1d94
jjanmo <jjanmo07@hanmail.net>
2023-12-13 00:37:38 +0900

    feat: change on-realtime fetching event

--- project03/src/components/TalkList/TalkList.tsx
+++ project03/src/components/TalkList/TalkList.tsx
@@ -1,6 +1,6 @@
 import { database } from '@config/firebase'
-import { collection, getDocs, orderBy, query } from 'firebase/firestore'
-import { useEffect, useState } from 'react'
+import { Unsubscribe, collection, getDocs, onSnapshot, orderBy, query } from 'firebase/firestore'
+import { useEffect, useRef, useState } from 'react'
 
 interface Koalang {
   id: string
@@ -12,17 +12,35 @@ interface Koalang {
 }
 
 export default function TalkList() {
-  const [koalangs, seKoalangs] = useState<Koalang[]>()
+  const [koalangs, setKoalangs] = useState<Koalang[]>()
+  const unsubscribeRef = useRef<Unsubscribe>(null)
 
   useEffect(() => {
     const fetchQuery = async () => {
       const koalangsQuery = query(collection(database, 'koalangs'), orderBy('createdAt', 'desc'))
-      const snapshots = await getDocs(koalangsQuery)
-      seKoalangs(
-        snapshots.docs.map((doc) => {
-          const { userId, user, message, image, createdAt, id } = doc.data()
+
+      // request once
+      // const snapshots = await getDocs(koalangsQuery)
+      // setKoalangs(
+      //   snapshots.docs.map((doc) => {
+      //     const { userId, user, message, image, createdAt } = doc.data()
+      //     return {
+      //       id : doc.id,
+      //       userId,
+      //       user,
+      //       message,
+      //       image,
+      //       createdAt,
+      //     }
+      //   })
+      // )
+
+      // realtime event
+      const unsubscribe = await onSnapshot(koalangsQuery, (snapshots) => {
+        const koalangs = snapshots.docs.map((doc) => {
+          const { userId, user, message, image, createdAt } = doc.data()
           return {
-            id,
+            id: doc.id,
             userId,
             user,
             message,
@@ -30,10 +48,17 @@ export default function TalkList() {
             createdAt,
           }
         })
-      )
+        setKoalangs(koalangs)
+      })
+      unsubscribeRef.current = unsubscribe
     }
 
     fetchQuery()
+
+    return () => {
+      unsubscribeRef.current()
+      console.log('cleanup!')
+    }
   }, [])
 
   return <div>{JSON.stringify(koalangs)}</div>
