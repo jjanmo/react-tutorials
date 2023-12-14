bc9e2c2a4213c6c68ad7622e33040504080d2daa
jjanmo <jjanmo07@hanmail.net>
2023-12-14 23:59:36 +0900

    feat: update avatar comp

---
+++ project03/src/pages/Profile/Profile.style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const AvatarContainer = styled.div``
+export const AvatarImageWrapper = styled.div`
+  width: 180px;
+  height: 180px;
+  border-radius: 50%;
+  overflow: hidden;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
--- project03/src/pages/Profile/Profile.tsx
+++ project03/src/pages/Profile/Profile.tsx
@@ -1,5 +1,20 @@
+import useAuthContext from '@context/auth'
+import DefaultAvatar from '@icons/DefaultAvatar'
+import * as S from './Profile.style'
 import * as CS from '../commom.style'
 
 export default function Profile() {
-  return <CS.Container>this is Profile page</CS.Container>
+  const { user } = useAuthContext()
+  console.log(user)
+  return (
+    <CS.Container>
+      <div>
+        <div>upload</div>
+        <S.AvatarImageWrapper>
+          {user?.photoURL ? <img src={user.photoURL} alt="avatar" /> : <DefaultAvatar size={140} />}
+        </S.AvatarImageWrapper>
+        <div>{user?.displayName || user?.email}</div>
+      </div>
+    </CS.Container>
+  )
 }
