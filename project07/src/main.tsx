2d54c1a13221f0205cb8b91f9e255b7e942cd37c
jjanmo <jjanmo07@hanmail.net>
2024-07-24 23:38:28 +0900

    feat: mui 설치

--- project07/src/main.tsx
+++ project07/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
+import CssBaseline from '@mui/material/CssBaseline'
 
 const router = createRouter({ routeTree })
 declare module '@tanstack/react-router' {
@@ -12,6 +13,7 @@ declare module '@tanstack/react-router' {
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <CssBaseline />
     <RouterProvider router={router} />
   </React.StrictMode>
 )
