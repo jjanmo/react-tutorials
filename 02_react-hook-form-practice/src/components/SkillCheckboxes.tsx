01984f7b830bdd6d044b969fcda41ae6cf121287
jjanmo <jjanmo07@hanmail.net>
2025-11-26 23:14:46 +0900

    feat: 스킬 checkbox 컴퍼넌트 구현

---
+++ 02_react-hook-form-practice/src/components/SkillCheckboxes.tsx
@@ -0,0 +1,43 @@
+import { Checkbox, FormControl, FormControlLabel, FormGroup, Stack, Typography } from '@mui/material';
+
+const SKILLS = ['Productive', 'Creative', 'Agile', 'Problem solving'];
+
+interface Props {
+  selectedSkills: string[];
+  onChange: (value: string[]) => void;
+}
+const SkillCheckboxes = ({ selectedSkills, onChange }: Props) => {
+  return (
+    <Stack direction="column">
+      <Typography component="label" htmlFor="skill-checkboxes" variant="body2" sx={{ display: 'inline-block' }}>
+        스킬
+      </Typography>
+      <FormControl>
+        <FormGroup sx={{ mb: 2 }}>
+          {SKILLS.map((skill) => (
+            <FormControlLabel
+              key={skill}
+              control={
+                <Checkbox
+                  checked={selectedSkills.includes(skill)}
+                  onChange={() =>
+                    onChange(
+                      selectedSkills.includes(skill)
+                        ? [...selectedSkills.filter((s) => s !== skill)]
+                        : [...selectedSkills, skill]
+                    )
+                  }
+                  size="small"
+                  sx={{ py: 0 }}
+                />
+              }
+              label={skill}
+            />
+          ))}
+        </FormGroup>
+      </FormControl>
+    </Stack>
+  );
+};
+
+export default SkillCheckboxes;
