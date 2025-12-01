38be0602cc4ed40101cd12f01ab15d965b23cb4e
jjanmo <jjanmo07@hanmail.net>
2025-12-01 23:54:45 +0900

    feat: zod 스키마 작성

---
+++ 02_react-hook-form-practice/src/constants/jobApplicationSchema.ts
@@ -0,0 +1,23 @@
+import dayjs from 'dayjs';
+import * as z from 'zod';
+import { LANGUAGES, REGIONS } from './jobApplicationForm';
+
+export const FormSchema = z.object({
+  name: z.string(),
+  email: z.email(),
+  region: z.enum(REGIONS),
+  language: z.enum(LANGUAGES),
+  gender: z.string(),
+  strengths: z.array(z.string()).min(1),
+  startDate: z
+    .string()
+    .refine((v) => dayjs(v).isValid(), 'Invalid date')
+    .transform((v) => dayjs(v)),
+  endDate: z
+    .string()
+    .refine((v) => dayjs(v).isValid(), 'Invalid date')
+    .transform((v) => dayjs(v)),
+  salaryRange: z.array(z.number()).min(1),
+});
+
+export type FormValues = z.infer<typeof FormSchema>;
