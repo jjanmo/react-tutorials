6903ba1b521deeff027f9f5997f25d2365fb372a
jjanmo <jjanmo07@hanmail.net>
2025-12-03 23:15:47 +0900

    feat: Gender,Language 선택 컴퍼넌트에 RHF 적용

--- 02_react-hook-form-practice/src/components/Fields/GenderRadioGroup.tsx
+++ 02_react-hook-form-practice/src/components/Fields/GenderRadioGroup.tsx
@@ -1,21 +1,37 @@
-import { Box, FormControlLabel, Radio, RadioGroup, Typography } from '@mui/material';
+import { Box, FormControlLabel, FormHelperText, Radio, RadioGroup, Typography } from '@mui/material';
+
+import type { FormData } from '@/types';
+import { Controller, type Control } from 'react-hook-form';
 
 interface Props {
-  selectedGender: string;
-  onChange: (value: string) => void;
+  control: Control<FormData>;
 }
 
-const GenderRadioGroup = ({ selectedGender, onChange }: Props) => {
+const GenderRadioGroup = ({ control }: Props) => {
   return (
-    <Box>
-      <Typography component="label" variant="body2" sx={{ display: 'inline-block' }}>
-        성별
-      </Typography>
-      <RadioGroup row value={selectedGender} onChange={(e) => onChange(e.target.value)}>
-        <FormControlLabel value="male" control={<Radio size="small" />} label="남성" />
-        <FormControlLabel value="female" control={<Radio size="small" />} label="여성" />
-      </RadioGroup>
-    </Box>
+    <Controller
+      control={control}
+      name="gender"
+      rules={{
+        required: '성별을 선택해주세요',
+      }}
+      render={({ field, fieldState }) => (
+        <Box>
+          <Typography component="label" variant="body2" sx={{ display: 'inline-block' }}>
+            성별
+          </Typography>
+          <RadioGroup row {...field}>
+            <FormControlLabel value="male" control={<Radio size="small" />} label="남성" />
+            <FormControlLabel value="female" control={<Radio size="small" />} label="여성" />
+          </RadioGroup>
+          {fieldState.error && (
+            <FormHelperText error sx={{ mt: -0.5 }}>
+              {fieldState.error.message}
+            </FormHelperText>
+          )}
+        </Box>
+      )}
+    />
   );
 };
 
--- 02_react-hook-form-practice/src/components/Fields/LanguageSelector.tsx
+++ 02_react-hook-form-practice/src/components/Fields/LanguageSelector.tsx
@@ -1,26 +1,41 @@
 import { LANGUAGES } from '@/constants/jobApplicationForm';
-import type { Language } from '@/types';
-import { Stack, ToggleButton, ToggleButtonGroup, Typography } from '@mui/material';
+import type { FormData } from '@/types';
+import { FormHelperText, Stack, ToggleButton, ToggleButtonGroup, Typography } from '@mui/material';
+import { Controller } from 'react-hook-form';
+import type { Control } from 'react-hook-form';
 
 interface Props {
-  selectedLanguage: Language;
-  onChange: (value: Language) => void;
+  control: Control<FormData>;
 }
 
-const LanguageSelector = ({ selectedLanguage, onChange }: Props) => {
+const LanguageSelector = ({ control }: Props) => {
   return (
-    <Stack direction="column">
-      <Typography component="label" variant="body2" sx={{ display: 'inline-block', mb: 0.5 }}>
-        주요 사용 언어
-      </Typography>
-      <ToggleButtonGroup exclusive value={selectedLanguage} onChange={(_e, val) => val && onChange(val)} size="small">
-        {LANGUAGES.map((language) => (
-          <ToggleButton key={language} value={language} sx={{ width: 114 }}>
-            {language}
-          </ToggleButton>
-        ))}
-      </ToggleButtonGroup>
-    </Stack>
+    <Controller
+      control={control}
+      name="language"
+      rules={{
+        required: '주요 사용 언어를 선택해주세요',
+      }}
+      render={({ field, fieldState }) => (
+        <Stack direction="column">
+          <Typography component="label" variant="body2" sx={{ display: 'inline-block', mb: 0.5 }}>
+            주요 사용 언어
+          </Typography>
+          <ToggleButtonGroup exclusive {...field} size="small">
+            {LANGUAGES.map((language) => (
+              <ToggleButton key={language} value={language} sx={{ width: 114 }}>
+                {language}
+              </ToggleButton>
+            ))}
+          </ToggleButtonGroup>
+          {fieldState.error && (
+            <FormHelperText error sx={{ mx: 1.5 }}>
+              {fieldState.error.message}
+            </FormHelperText>
+          )}
+        </Stack>
+      )}
+    />
   );
 };
 
