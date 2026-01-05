bc9a1e6dc7b396fe6ee9a6634fe3b9db6d657408
jjanmo <jjanmo07@hanmail.net>
2026-01-05 23:36:22 +0900

    fix: plugins 설정 순서 오류 수정

--- 02_react-hook-form-practice/vite.config.ts
+++ 02_react-hook-form-practice/vite.config.ts
@@ -6,14 +6,14 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
-    }),
-    tanstackRouter({
-      target: 'react',
-      autoCodeSplitting: true,
     }),
   ],
   resolve: {
