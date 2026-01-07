6405060c1d90b1a3414073880bcad0c699c291e0
jjanmo <jjanmo07@hanmail.net>
2026-01-07 23:56:01 +0900

    refactor: 컴퍼넌트 폴더 구조 변경

--- 02_react-hook-form-practice/src/routes/index.tsx
+++ 02_react-hook-form-practice/src/routes/index.tsx
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <div>
       <Link to="/forms/job-application">Job Application</Link>
+      <Link to="/forms/basic-form">Basic Form</Link>
     </div>
   );
 };
